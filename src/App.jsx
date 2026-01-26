e6398cdbf182da153d2249b765b5fe8098d3e768
Himanshu <hs2947095@gmail.com>
2026-01-26 21:05:28 +0530

    this is color checking for my text

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,12 @@ const App = () => {
   return (
     <div>
       <h1 className="text-4xl font-bold text-center">Hello Himanshu</h1>
-      <p className="text-2xl font-medium text-center">I am learning Git</p>
+      <p className="text-2xl font-medium text-center text-purple-600">
+        I am learning Git
+      </p>
+      <h3 className="text-2xl font-semibold text-center text-amber-500">
+        hello this is testing from me for git knowledge
+      </h3>
     </div>
   );
 };
