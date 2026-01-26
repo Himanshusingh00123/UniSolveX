2a349b4c4d89c0e2ae78af81520f141b6fb5e8f2
Himanshu <hs2947095@gmail.com>
2026-01-26 20:04:50 +0530

    i add my name and what i learning

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 const App = () => {
   return (
-    <div >
-    
+    <div>
+      <h1 className="text-4xl font-bold text-center">Hello Himanshu</h1>
+      <p className="text-2xl font-medium text-center">I am learning Git</p>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
