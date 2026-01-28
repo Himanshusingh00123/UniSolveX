4612878c684a4ce9bd6bbc83b9b124b03fda7a0f
Himanshu <hs2947095@gmail.com>
2026-01-28 22:58:31 +0530

    i add admin panel button in home page

--- src/Components/LandingPage/Home.jsx
+++ src/Components/LandingPage/Home.jsx
@@ -1,7 +1,13 @@
+import { Link } from "react-router-dom";
 const Home = () => {
   return (
     <div>
-      <h1 className="text-center text-5xl font-bold"> hello himanshu</h1>
+      <h1 className="text-center text-5xl font-bold my-10"> hello himanshu</h1>
+     <Link to="/admin">
+      <button className="border p-3 items-center m-auto  justify-center hover:bg-blue-500 hover:text-white text-lg font-bold rounded-full  flex">
+        Go to admin
+      </button>
+     </Link>
     </div>
   );
 };
