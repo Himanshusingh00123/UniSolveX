ce7e72cea7ca818172df9765f44f07d865898096
Himanshu <hs2947095@gmail.com>
2026-02-18 22:34:31 +0530

    i enhance responsive

--- src/Components/Admin/Dashboard.jsx
+++ src/Components/Admin/Dashboard.jsx
@@ -5,11 +5,11 @@ const Dashboard = () => {
         Admin Dashboard
       </h1>
       <div
-        className="md:bg-white md:h-50 sm:h-80 h-screen md:border-1 
+        className="md:bg-white  md:h-48 sm:h-80 h-screen  md:border
                   md:shadow-sm border-gray-300 rounded-xl grid
-                  md:grid-cols-4 sm:grid-cols-2 grid-cols-1 p-3 gap-3 "
+                  xl:grid-cols-4 md:grid-cols-2 sm:grid-cols-2 grid-cols-1 p-3 gap-3 "
       >
-        <div className="bg-linear-to-br from-slate-900 via-slate-800 to-slate-900  cursor-pointer hover:scale-103 duration-400 transition  rounded-xl shadow-lg px-5 py-4">
+        <div className="auto-rows-fr bg-linear-to-br from-slate-900 via-slate-800 to-slate-900   cursor-pointer hover:scale-103 duration-400 transition  rounded-xl shadow-lg px-5 py-4 ">
           <h1 className="text-3xl text-gray-300 font-semibold mb-2">
             <i class="ri-school-line mr-2 text-gray-200"></i>Total Courses
           </h1>
@@ -44,7 +44,7 @@ const Dashboard = () => {
             Total Papers
           </h1>
           <h3 className="text-5xl font-bold text-gray-300">25</h3>
-           <div className=" flex text-xl justify-end items-center mt-2 ">
+          <div className=" flex text-xl justify-end items-center mt-2 ">
             <h4 className="text-green-500 text-2xl font-semibold mr-3">
               <i class="ri-line-chart-fill mr-3"></i>29%
             </h4>
@@ -57,7 +57,7 @@ const Dashboard = () => {
             Total Solutions
           </h1>
           <h3 className="text-5xl font-bold text-gray-300 ">22</h3>
-           <div className=" flex text-xl justify-end items-center mt-2 ">
+          <div className=" flex text-xl justify-end items-center mt-2 ">
             <h4 className="text-green-500 text-2xl font-semibold mr-3">
               <i class="ri-line-chart-fill mr-3"></i>19%
             </h4>
