21276c26c13e43d3c612e44e65d66c9fa1a249c2
Himanshu <hs2947095@gmail.com>
2026-03-05 20:59:14 +0530

    i design button for course and make it responsive

--- src/Components/Admin/Courses.jsx
+++ src/Components/Admin/Courses.jsx
@@ -1,7 +1,19 @@
+import { IoMdAdd } from "react-icons/io";
+
 const Courses = () => {
   return (
     <div>
-      <h1 className="text-5xl font-bold  text-center my-5">Courses</h1>
+      <div className="sm:flex max-sm:flex-col  sm:justify-between items-center sm:py-3 sm:px-4 py-2 px-3">
+        <h1 className="sm:text-3xl text-2xl font-semibold pb-2.5 text-gray-700">
+          Manage Courses
+        </h1>
+        <button
+          className="bg-[#0d6efd] flex justify-center max-sm:w-full  gap-1.5 items-center px-4 py-2  hover:bg-[#0b5ed7] hover:text-white 
+          sm:text-base text-lg text-gray-100 font-semibold shadow-md cursor-pointer rounded-lg"
+        >
+          <IoMdAdd className="text-lg  font-bold" /> Add Course
+        </button>
+      </div>
     </div>
   );
 };
