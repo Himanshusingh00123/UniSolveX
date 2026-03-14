4155115ea368152b4e43d2b072edc75dd2eb0cff
Himanshu <hs2947095@gmail.com>
2026-03-14 12:35:31 +0530

    i design full setting page and make it responsive

--- src/Components/Admin/Setting.jsx
+++ src/Components/Admin/Setting.jsx
@@ -1,6 +1,7 @@
 import { GrUserManager } from "react-icons/gr";
 import { FiSave } from "react-icons/fi";
 import { useState } from "react";
+import { TbLockPassword } from "react-icons/tb";
 
 const Setting = () => {
   const [bg, setBg] = useState(true);
@@ -16,7 +17,10 @@ const Setting = () => {
         </p>
       </div>
 
-      <div className="bg-white border border-gray-300 -mt-5 sm:mt-0 rounded-xl p-5 sm:w-7/12 h-85">
+      {/* 
+-------------------------------------------------------------------------profile section--------------------------------------- */}
+
+      <div className="bg-white border border-gray-300 -mt-5 sm:mt-0 rounded-xl sm:p-6 p-5 sm:w-7/12 h-85">
         <form className="grid gap-1 h-2/12">
           <h1 className="text-2xl text-gray-700 font-semibold flex items-center gap-2">
             <GrUserManager className="text-indigo-500" />
@@ -60,7 +64,62 @@ const Setting = () => {
         </form>
       </div>
 
-      <div className="bg-white border border-gray-300 rounded-xl sm:mt-4 mt-2 sm:w-7/12 h-120"></div>
+      {/* ----------------------------------------------------------password section------------------------------------------ */}
+
+      <div className="bg-white border border-gray-300 rounded-xl sm:mt-4 mt-2 sm:p-6 p-5 sm:w-7/12 h-114 sm:h-110">
+        <form className="grid gap-1 h-2/12">
+          <h1 className="text-2xl text-gray-700 font-semibold flex items-center gap-2">
+            <TbLockPassword className="text-indigo-500" />
+            Change Password
+          </h1>
+          <p className="text-sm  text-gray-400 font-medium tracking-wide">
+            Update your password to keep your account secure
+          </p>
+
+          <label className="text-base text-gray-800 font-semibold mt-4">
+            Current Password
+          </label>
+          <input
+            className="border border-gray-300 rounded-lg 
+                      p-2 bg-gray-100 focus:outline-blue-500 focus:outline-2"
+            type="password"
+            placeholder="Enter Your Current Password"
+            required
+          />
+
+          <label className="text-base text-gray-800 font-semibold mt-4">
+            New Password
+          </label>
+          <input
+            className="border border-gray-300 rounded-lg 
+                      p-2 bg-gray-100 focus:outline-blue-500 focus:outline-2"
+            type="password"
+            placeholder="Enter Your New Password"
+            required
+          />
+
+          <label className="text-base text-gray-800 font-semibold mt-4">
+            Confirm New Password
+          </label>
+          <input
+            className="border border-gray-300 rounded-lg 
+                      p-2 bg-gray-100 focus:outline-blue-500 focus:outline-2"
+            type="password"
+            placeholder="Confirm Your New Password"
+            required
+          />
+
+          <button
+            onClick={() => setBg(!bg)}
+            className={`${bg ? "bg-linear-to-b from-blue-500 to-blue-700" : "bg-linear-to-b from-blue-400 to-blue-600"}
+             text-white font-semibold sm:w-4/12 hover:from-blue-600 hover:to-blue-800
+               px-3.5 py-2.5 flex items-center justify-center gap-2 cursor-pointer rounded-lg mt-4`}
+          >
+            <TbLockPassword className="text-lg" />
+            Change Password
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
