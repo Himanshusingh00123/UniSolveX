664912ac05ad9ed1e466c20cd5a6e6809181e2ae
Himanshu <hs2947095@gmail.com>
2026-03-19 20:33:51 +0530

    i change some part like no course found and some others

--- src/Components/Admin/Courses.jsx
+++ src/Components/Admin/Courses.jsx
@@ -49,15 +49,13 @@ const Courses = () => {
       <div className="bg-white py-3 px-4 sm:rounded-lg sm:border-2 shadow-md border-gray-300 h-full">
         <div className="grid  sm:grid-cols-2  sm:gap-4 gap-2 mb-4">
           <div>
-            <label className="text-lg text-gray-500 font-medium">
-              Category
-            </label>
+            <label className="text-lg text-gray-500 font-medium">Course</label>
             <select className="w-full bg-gray-100 focus:outline-blue-600 focus:outline-2 border-2 cursor-pointer border-gray-300 rounded-lg px-4 py-2 mt-1 text-base text-gray-500 font-semibold">
               <option
                 className="sm:text-base text-xs text-gray-600 font-medium "
                 value="all"
               >
-                All Categories
+                All Courses
               </option>
               {course.map((item, index) => (
                 <option
@@ -97,65 +95,71 @@ const Courses = () => {
 
         {/* -----------------------------------------------Course Table----------------------------------------------------------------- */}
 
-        <div className="border-2 border-gray-300 rounded-xl shadow-sm px-3 overflow-x-auto">
-          <table className="w-full text-center border-separate border-spacing-y-2">
-            <thead className="bg-emerald-600 text-white  whitespace-nowrap">
-              <th className="p-2 rounded-l-lg px-3 w-3/12 tracking-wide">
-                Course Name
-              </th>
-              <th className="border-l px-3 border-gray-300 w-4/12 tracking-wide">
-                Full Course Title
-              </th>
-              <th className="border-l px-3 border-gray-300  tracking-wide">
-                Status
-              </th>
-              <th className="border-l border-gray-300 w-3/12 rounded-r-lg tracking-wide">
-                Action
-              </th>
-            </thead>
+        {course.length > 0 ? (
+          <div className="border-2 border-gray-300 rounded-xl shadow-sm px-3 overflow-x-auto">
+            <table className="w-full text-center border-separate border-spacing-y-2">
+              <thead className="bg-emerald-600 text-white  whitespace-nowrap">
+                <th className="p-2 rounded-l-lg px-3 w-3/12 tracking-wide">
+                  Course Name
+                </th>
+                <th className="border-l px-3 border-gray-300 w-4/12 tracking-wide">
+                  Full Course Title
+                </th>
+                <th className="border-l px-3 border-gray-300  tracking-wide">
+                  Status
+                </th>
+                <th className="border-l border-gray-300 w-3/12 rounded-r-lg tracking-wide">
+                  Action
+                </th>
+              </thead>
 
-            <tbody className="text-gray-600 text-center text-sm sm:text-base font-semibold whitespace-nowrap">
-              {course.map((item, index) => (
-                <tr
-                  key={index}
-                  className="odd:bg-linear-to-r odd:from-gray-200 odd:to-white even:bg-white"
-                >
-                  <td className=" rounded-l-lg font-bold">{item.course}</td>
-                  <td>{item.full_title}</td>
-                  <td>
-                    <span
-                      className={`${item.status === "Active" ? "bg-green-200 text-green-700" : "bg-red-200 text-red-700"} px-2.5 py-0.5 rounded-lg `}
-                    >
-                      {item.status}
-                    </span>
-                  </td>
-                  <td className="flex justify-center items-center p-1.5 text-white gap-2 rounded-r-xl">
-                    <div
-                      className="bg-linear-to-b from-blue-500 to-blue-700 py-1 px-3.5 hover:from-blue-600 hover:to-blue-800
+              <tbody className="text-gray-600 text-center text-sm sm:text-base font-semibold whitespace-nowrap">
+                {course.map((item, index) => (
+                  <tr
+                    key={index}
+                    className="odd:bg-linear-to-r odd:from-gray-200 odd:to-white even:bg-white"
+                  >
+                    <td className=" rounded-l-lg font-bold">{item.course}</td>
+                    <td>{item.full_title}</td>
+                    <td>
+                      <span
+                        className={`${item.status === "Active" ? "bg-green-200 text-green-700" : "bg-red-200 text-red-700"} px-2.5 py-0.5 rounded-lg `}
+                      >
+                        {item.status}
+                      </span>
+                    </td>
+                    <td className="flex justify-center items-center p-1.5 text-white gap-2 rounded-r-xl">
+                      <div
+                        className="bg-linear-to-b from-blue-500 to-blue-700 py-1 px-3.5 hover:from-blue-600 hover:to-blue-800
                             shadow-sm rounded-md flex justify-center items-center gap-2 cursor-pointer
                             transition-all duration-300 hover:scale-105"
-                    >
-                      <FaEdit />
-                      Edit
-                    </div>
-                    <div
-                      className="bg-linear-to-b from-red-500 to-red-700 py-1 px-3.5 hover:from-red-600 hover:to-red-800
+                      >
+                        <FaEdit />
+                        Edit
+                      </div>
+                      <div
+                        className="bg-linear-to-b from-red-500 to-red-700 py-1 px-3.5 hover:from-red-600 hover:to-red-800
                             shadow-sm rounded-md flex justify-center items-center gap-1.5 cursor-pointer
                             transition-all duration-300 hover:scale-105"
-                    >
-                      <RiDeleteBin5Fill />
-                      Delete
-                    </div>
-                  </td>
-                </tr>
-              ))}
+                      >
+                        <RiDeleteBin5Fill />
+                        Delete
+                      </div>
+                    </td>
+                  </tr>
+                ))}
 
-              <p className="text-xs  text-gray-500 sm:hidden mb-1">
-                ← Scroll horizontally to see more →
-              </p>
-            </tbody>
-          </table>
-        </div>
+                <p className="text-xs  text-gray-500 sm:hidden mb-1">
+                  ← Scroll horizontally to see more →
+                </p>
+              </tbody>
+            </table>
+          </div>
+        ) : (
+          <div className="h-30 flex justify-center items-center text-lg font-medium text-gray-500 tracking-wide">
+            No courses found. Add your first course to get started.
+          </div>
+        )}
       </div>
     </div>
   );
